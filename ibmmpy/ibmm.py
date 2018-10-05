7396be4353d18ada117486cfba8e221222e695ed
Reuben Aronson <rmaronson@cmu.edu>
2018-10-05 14:02:29 -0400

    finish first past

--- ibmmpy/ibmm.py
+++ ibmmpy/ibmm.py
@@ -100,9 +100,101 @@ class EyeClassifier:
     
     @staticmethod
     def postprocess(labels):
+        """
+        Post-process label assignments to clean up noise related stuff.
+        
+        For now, all this does is finds sequences ABA where B != A, A != noise, and converts them to AAA.
+        
+        This could be more sophisticated someday.
+        """
+        labels_to_fix = np.logical_and(
+                np.logical_and(labels[0:-2] == labels[2:], labels[0:-2] != labels[1:-1]),
+                labels[0:-2] != EyeClassifier.LABEL_NOISE)
+        indices = np.nonzero(labels_to_fix)
+        labels[indices+1] = labels[indices]
+        
+    @staticmethod
+    def fuse(labels0, labels1, ts=None, dt=None):
+        """
+        Fuse two different label sets to come to an agreement.
+        
+        Algorithm, roughly:
+            For each period n*dt - (n+1)*dt:
+                Find labels from each set within the period
+                Result label = majority vote among SAC, FIX; NSE if all are noise; break ties as SAC
+        
+        Arguments:
+        labels0 -- pandas-style dataframe with columns 'timestamp' and 'label', as determined by EyeClassifier.postprocess
+        labels1 -- second set as in labels0, e.g. from the other eye
+        ts -- list of timestamps to sample at, or None to use automatic samples from dt
+        dt -- sampling period to use, if ts is None
+        
+        Returns:
+        pandas DataFrame with columns:
+            timestamp -- ts or times generated from dt
+            label -- the fused labels
+        """
+        if ts is None:
+            ts = np.arange( min(labels0.timestamp[0], labels1.timestamp[0]), max(labels0.timestamp[-1], labels1.timestamp[-1]), dt)
+        fused_labels = np.zeros(ts.shape) + EyeClassifier.LABEL_NOISE
+        for idx in range(ts.size):
+            tprev = ts[idx-1] if idx > 0 else -np.inf
+            tnext = ts[idx]
+            cur_labels = np.concatenate( ( labels0.label[np.logical_and(labels0.timestamp >= tprev, labels0.timestamp < tnext)],
+                                           labels1.label[np.logical_and(labels1.timestamp >= tprev, labels1.timestamp < tnext)]
+                                            ) )
+            ct_sac = np.count_nonzero(cur_labels == EyeClassifier.LABEL_SAC)
+            ct_fix = np.count_nonzero(cur_labels == EyeClassifier.LABEL_FIX)
+            if ct_fix > ct_sac:
+                fused_labels[idx] = EyeClassifier.LABEL_FIX
+            elif ct_sac > ct_fix or ct_sac > 0:# they're equal and it's not entirely noise
+                fused_labels[idx] = EyeClassifier.LABEL_FIX
+            # otherwise leave as noise
+        
+        return pd.DataFrame({'timestamp': ts, 'label': fused_labels})
+        
     
-    def predict(self, data0, data1):
-        labels0 = EyeClassifier._predict(self.bmm_eye0, self.eye0_labels, data0)
-        labels1 = EyeClassifier._predict(self.bmm_eye1, self.eye1_labels, data1)
+    def predict(self, data0, data1, ts=None, dt=None):
+        labels0 = EyeClassifier.postprocess(EyeClassifier._predict(self.bmm_eye0, self.eye0_labels, data0))
+        labels1 = EyeClassifier.postprocess(EyeClassifier._predict(self.bmm_eye1, self.eye1_labels, data1))
+        return EyeClassifier.fuse(
+                pd.DataFrame({'timestamp': data0.timestamp, 'label': labels0}),
+                pd.DataFrame({'timestamp': data1.timestamp, 'label': labels1}),
+                ts, dt
+            )
+        
+    @staticmethod
+    def get_fixations_from_labels(labels, gaze_data=None, min_fix_dur=100):
+        """
+        Convert a sequence of labels into detected fixations.
+        
+        Arguments:
+        labels -- pandas-style dataframe with columns 'timestamp', 'label', as in supplied by EyeClassifier.fuse
+        gaze_data -- Pandas-style dataframe with columns 'timestamp', 'x', 'y', or None. Fills out fixation 'x', 'y' if provided
+        min_fix_dur -- minimum fixation duration to filter out (in ms), or None if no filtering is to be done
+        
+        Returns:
+        pandas dataframe of all detected fixations with columns 'start_timestamp', 'duration' (in ms). If gaze_data is provided, also
+        includes columns 'x', 'y', which are the mean of the values of gaze_data.x and gaze_data.y for the duration of the fixation
+        """
+        is_fix = labels.label.values == EyeClassifier.LABEL_FIX
+        fix_change = is_fix[1:] - is_fix[:-1]
+        fix_start = np.nonzero(fix_change == 1) + 1
+        if is_fix[0]:
+            fix_start = np.concatenate( (0, fix_start) )
+        fix_end = np.nonzero(fix_change == -1)
+        if is_fix[-1]:
+            fix_end = np.concatenate( (fix_end, is_fix.size-1))
+        
+        fix = pd.DataFrame({ 'start_timestamp': labels.timestamp[fix_start], 'duration': labels.timestamp[fix_end] - labels.timestamp[fix_start]})
+        if min_fix_dur is not None:
+            fix = fix[fix.duration >= min_fix_dur, :]
+        if gaze_data is not None:
+            fix.assign(x = lambda r: np.mean( gaze_data.x[np.logical_and(gaze_data.timestamp >= r.start_timestamp,
+                                                                       gaze_data.timestamp <= r.start_timestamp + .001*r.duration)] ))
+            fix.assign(y = lambda r: np.mean( gaze_data.y[np.logical_and(gaze_data.timestamp >= r.start_timestamp,
+                                                                       gaze_data.timestamp <= r.start_timestamp + .001*r.duration)] ))
+        return gaze_data
+        
         
 
